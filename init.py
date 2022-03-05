d0f2c49ff2ce3d8e992f06bade3fc1506d834524
r2rahul <katyayan.rahul@gmail.com>
2022-03-05 23:17:57 +0530

    reorganized teh folder

--- init.py
+++ init.py
@@ -4,7 +4,7 @@ import requests # library to handle requests
 from bs4 import BeautifulSoup # library to parse HTML documents
 import pandas_profiling as pp
 
-from etl import clean_text, create_wordcloud
+from py.etl import clean_text, create_wordcloud
 # %%
 with pd.HDFStore("data/thematic_20220305.h5") as store:
     data = store["desc"]
